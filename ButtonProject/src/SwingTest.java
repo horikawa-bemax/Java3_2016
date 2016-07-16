264bc0b2a7afb7cea506a187d39560b1a0b82a23
Masaaki Horikawa <horikawa@bemax.jp>
2016-07-16 16:10:32 +0900

    画像追加

---
+++ ButtonProject/src/SwingTest.java
@@ -0,0 +1,14 @@
+package swingtest;
+
+import javax.swing.*;
+
+public class SwingTest{
+	public static void main(String[] args){
+		JFrame f = new JFrame();
+		f.setSize(300,300);
+
+		f.setVisible(true);
+
+	}
+}
+
